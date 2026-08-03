0c6ef37c375a9bff9be844c924f58e5f2df8d177
Santosh Kumar Pasi <santoshkumarpasi@Santoshs-MacBook-Air.local>
2026-08-03 19:32:31 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/santosh/nutrition_analyzer/NutritionAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.santosh.nutrition_analyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutritionAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutritionAnalyzerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/santosh/nutrition_analyzer/NutritionAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santosh.nutrition_analyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutritionAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
