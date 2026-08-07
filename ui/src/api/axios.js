9e66048aba3ab8070154b55b3e500d0d4d643639
Santosh Kumar Pasi <santoshkumarpasi@Santoshs-MacBook-Air.local>
2026-08-08 01:25:57 +0530

    Configure frontend for production

--- ui/src/api/axios.js
+++ ui/src/api/axios.js
@@ -1,24 +1,26 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:8080/api",
-    headers: {
-        "Content-Type": "application/json",
-    },
+	baseURL: import.meta.env.VITE_API_URL,
+	headers: {
+		"Content-Type": "application/json",
+	},
 });
 
 // Add JWT to every request
 api.interceptors.request.use(
-    (config) => {
-        const token = localStorage.getItem("token");
+	(config) => {
 
-        if (token) {
-            config.headers.Authorization = `Bearer ${token}`;
-        }
+		const token = localStorage.getItem("token");
 
-        return config;
-    },
-    (error) => Promise.reject(error)
+		if (token) {
+			config.headers.Authorization = `Bearer ${token}`;
+		}
+
+		return config;
+
+	},
+	(error) => Promise.reject(error)
 );
 
 export default api;
